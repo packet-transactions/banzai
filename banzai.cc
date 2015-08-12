113caa828763caa394cd4a8b7de0a6d2882db654
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-12 09:33:03 -0700

    Specify input and output packet

--- banzai.cc
+++ banzai.cc
@@ -7,16 +7,17 @@
 #include <iostream>
 
 int main(const int argc __attribute__ ((unused)), const char ** argv __attribute__((unused))) {
-  /// Initialize field map to contain the right fields
+  // Initialize field map to contain the right fields
   const Packet exemplar_packet(std::map<std::string, uint32_t>({{"a", 0}, {"b", 0}, {"c", 0}}), 1);
 
-  /// Generate random packets using this exemplar
-  const auto random_packet = exemplar_packet.generate_random_packet();
-  std::cerr << "random_packet is " << random_packet << "\n";
-
-  /// Initialized pipeline
+  // Initialize pipeline
   Pipeline pipeline({{Atom([](const Packet & p, State & s __attribute__((unused))) { return p; }, FieldContainer())}});
 
-  /// Run for 100 ticks
-  for (uint32_t i = 0; i < 100; i++) std::cerr << pipeline.tick(exemplar_packet.generate_random_packet());
+  // Run for 100 ticks
+  for (uint32_t i = 0; i < 100; i++) {
+    // Generate packets using this exemplar
+    const auto input_packet = exemplar_packet.generate_random_packet();
+    std::cerr << "input_packet is \n" << input_packet << "\n";
+    std::cerr << "output_packet is \n" << pipeline.tick(input_packet);
+  }
 }
