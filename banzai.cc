d1c4ef4cd7a43358debd010a4cc50d1f4cbc589b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-12 15:02:01 -0700

    Supply arguments on command line

--- banzai.cc
+++ banzai.cc
@@ -9,12 +9,19 @@
 
 int main(const int argc __attribute__ ((unused)), const char ** argv __attribute__((unused))) {
   try {
-    // Construct shared library loader
-    DynamicLinkingLoader dynamic_linking_loader(".libs/libprogtorun.so.0.0.0");
+    if (argc < 4) {
+      std::cerr << "Usage: " << argv[0] << " prog_to_run_as_library pipeline_name exemplar_packet_name " << std::endl;
+      return EXIT_SUCCESS;
+    }
+    // Get cmdline args
+    const auto prog_to_run(argv[1]);
+    const auto pipeline_name(argv[2]);
+    const auto packet_name(argv[3]);
 
-    // Construct pipeline and exemplar packet from shared library.
-    Pipeline  pipeline = dynamic_linking_loader.get_object<Pipeline>("test_pipeline");
-    const Packet exemplar_packet = dynamic_linking_loader.get_object<Packet>("test_packet");
+    // Construct shared library loader for prog_to_run and use this to load packet_name and pipeline_name
+    DynamicLinkingLoader dynamic_linking_loader(prog_to_run);
+    Pipeline  pipeline = dynamic_linking_loader.get_object<Pipeline>(pipeline_name);
+    const Packet exemplar_packet = dynamic_linking_loader.get_object<Packet>(packet_name);
 
     // Run for 100 ticks
     for (uint32_t i = 0; i < 100; i++) {
