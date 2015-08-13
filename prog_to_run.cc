42a343e9ae9b918a20a65f71327ad3e15a9dfa3c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-12 19:44:29 -0700

    Two distinct atoms in a two-stage pipeline

--- prog_to_run.cc
+++ prog_to_run.cc
@@ -3,6 +3,11 @@
 #include "pipeline.h"
 
 extern "C" {
+  Packet plus20(const Packet & p, State & s __attribute__((unused))) {
+    auto ret = p;
+    ret.write("a", ret.read("a") + 20);
+    return ret;
+  }
   Packet plus10(const Packet & p, State & s __attribute__((unused))) {
     auto ret = p;
     ret.write("a", ret.read("a") + 10);
@@ -10,5 +15,5 @@ extern "C" {
   }
   Packet test_packet(std::map<std::string, uint32_t>({{"a", 0}, {"b", 0}, {"c", 0}}), 1);
 
-  Pipeline test_pipeline({{Atom(plus10, FieldContainer())}, {Atom(plus10, FieldContainer())}});
+  Pipeline test_pipeline({{Atom(plus10, FieldContainer())}, {Atom(plus20, FieldContainer())}});
 }
