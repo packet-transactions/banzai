bb8dd8406427e6569628fbc342813dbba559fe43
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-12 19:11:23 -0700

    Replace anonymous function with named one and make sure it works

--- prog_to_run.cc
+++ prog_to_run.cc
@@ -3,8 +3,12 @@
 #include "pipeline.h"
 
 extern "C" {
+  Packet plus10(const Packet & p, State & s __attribute__((unused))) {
+    auto ret = p;
+    ret.write("a", ret.read("a") + 10);
+    return ret;
+  }
   Packet test_packet(std::map<std::string, uint32_t>({{"a", 0}, {"b", 0}, {"c", 0}}), 1);
 
-  Pipeline test_pipeline({{Atom([](const Packet & p, State & s __attribute__((unused)))
-                               { auto ret = p; ret.write("a", ret.read("a") + 1); return ret; }, FieldContainer())}});
+  Pipeline test_pipeline({{Atom(plus10, FieldContainer())}});
 }
