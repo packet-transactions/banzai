be6ef0f71edaf0cf98297efbe57ecb4f94bb1877
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-14 09:36:16 -0700

    Replace prog_to_run.cc with version generated by domino (as of commit: anirudhsk/domino@018f4ded06710360fd7cc3bbe6f6df1e63d3dcfe)

--- prog_to_run.cc
+++ prog_to_run.cc
@@ -1,24 +1,16 @@
 #include "packet.h"
 #include "atom.h"
 #include "pipeline.h"
-
 extern "C" {
-  void atom0(Packet & packet, State & state __attribute__((unused))) {
-      packet("new_hop") = (packet("sport") * packet("dport")) % 10;
-      if (packet("arrival_time") - state("last_time") > 5) {
-        state("next_hop") = packet("new_hop");
-      };
-      state("last_time") = packet("arrival_time");
-      packet("next_hop") = state("next_hop");
-  }
-
-  void plus20(Packet & p, State & s __attribute__((unused))) {
-    p("a") = p("a") + 20;
-  }
-  void plus10(Packet & p, State & s __attribute__((unused))) {
-    p("a") = p("a") + 10;
-  }
-  PacketFieldSet test_fields({"new_hop", "next_hop", "sport", "dport"});
-
-  Pipeline test_pipeline({{Atom(atom0, FieldContainer())}, {Atom(plus20, FieldContainer())}});
+void atom0(Packet &packet, State &state __attribute__((unused))) {
+  packet("new_hop") = (packet("sport") * packet("dport")) % 10;
+  if (packet("arrival_time") - state("last_time") > 5) {
+    state("next_hop") = packet("new_hop");
+  };
+  state("last_time") = packet("arrival_time");
+  packet("next_hop") = state("next_hop");
+}
+PacketFieldSet test_fields({"arrival_time", "dport", "new_hop", "next_hop",
+                            "sport"});
+Pipeline test_pipeline{{Atom(atom0, FieldContainer())}};
 }
