9623d6c123c387bf6de15de03465b9896c97f38c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-08-15 21:13:45 -0700

    Specify field set on command line, otherwise the spec and impl. spuriously diverge

--- banzai.cc
+++ banzai.cc
@@ -6,16 +6,30 @@
 #include <exception>
 #include <vector>
 #include <iostream>
+#include <regex>
+
+/// Split string based on another string used as delimiter
+/// using C++11's regex_token_iterator
+/// Based on http://en.cppreference.com/w/cpp/regex/regex_token_iterator
+/// and http://stackoverflow.com/a/9437426/1152801
+std::vector<std::string> split(const std::string & input, const std::string & regex_str) {
+  std::regex regex_object(regex_str);
+  std::sregex_token_iterator first{input.begin(), input.end(), regex_object, -1}, last;
+  return {first, last};
+}
 
 int main(const int argc __attribute__ ((unused)), const char ** argv __attribute__((unused))) {
   try {
-    if (argc < 3) {
-      std::cerr << "Usage: " << argv[0] << " prog_to_run_as_library seed" << std::endl;
+    if (argc < 4) {
+      std::cerr << "Usage: " << argv[0] << " prog_to_run_as_library seed comma_separated_test_field_list" << std::endl;
       return EXIT_SUCCESS;
     }
+
     // Get cmdline args
     const auto prog_to_run(argv[1]);
     const uint32_t seed = static_cast<uint32_t>(std::atoi(argv[2]));
+    const std::vector<std::string> field_vector = split(std::string(argv[3]), ",");
+    const PacketFieldSet packet_field_set(field_vector.begin(), field_vector.end());
 
     /// PRNG to generate random packet fields,
     std::default_random_engine prng = std::default_random_engine(seed);
@@ -26,7 +40,6 @@ int main(const int argc __attribute__ ((unused)), const char ** argv __attribute
     // Construct shared library loader for prog_to_run
     DynamicLinkingLoader dynamic_linking_loader(prog_to_run);
     Pipeline  pipeline = dynamic_linking_loader.get_object<Pipeline>("test_pipeline");
-    const PacketFieldSet packet_field_set = dynamic_linking_loader.get_object<PacketFieldSet>("test_fields");
 
     // Run for 100 ticks
     for (uint32_t i = 0; i < 100; i++) {
