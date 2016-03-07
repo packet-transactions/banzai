4ebc015330ad73c3c221a3c01aa6cd94d6d1d373
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-03-06 23:11:16 -0500

    Move everything into dynamic_match_action_table.h

--- dynamic_match_action_table.h
+++ dynamic_match_action_table.h
@@ -1,11 +1,14 @@
 #ifndef DYNAMIC_MATCH_ACTION_TABLE_H_
 #define DYNAMIC_MATCH_ACTION_TABLE_H_
 
+#include <cassert>
+
 #include <map>
 #include <vector>
 #include <string>
 #include <functional>
 
+#include "field_container.h"
 #include "packet.h"
 
 /// A match-action table supporting data-plane inserts and deletes of match keys.
@@ -24,17 +27,46 @@ class DynamicMatchActionTable {
   /// denoting the exact match fields
   DynamicMatchActionTable(const PacketFieldSet & t_field_names,
                           const Updater & t_updater,
-                          const Initializer & t_initializer);
+                          const Initializer & t_initializer)
+      : storage_(),
+        field_names_(t_field_names),
+        updater_(t_updater),
+        initializer_(t_initializer) {}
 
   /// Update entry:
   /// 1. Create a new entry if it doesn't exist.
   /// 2. Update the existing entry if it does exist.
   /// In both cases, return updated StateType value
-  StateType update(const Packet & packet);
+  StateType update(const Packet & packet) {
+    FieldContainer<int> sliced_packet;
+    for (const auto & field : field_names_) {
+      sliced_packet(field) = packet(field);
+    }
+  
+    if (storage_.find(sliced_packet) != storage_.end()) {
+      updater_(packet, storage_.at(sliced_packet));
+    } else {
+      storage_[sliced_packet] = initializer_(packet);
+    }
+  
+    return storage_.at(sliced_packet);
+  } 
+
 
   /// Erase entry, making sure that it actually exists
   /// Return the erased entry
-  StateType erase(const Packet & packet);
+  StateType erase(const Packet & packet) {
+    FieldContainer<int> sliced_packet;
+    for (const auto & field : field_names_) {
+      sliced_packet(field) = packet(field);
+    }
+
+    assert(storage_.find(sliced_packet) != storage_.end());
+    const auto ret = storage_.at(sliced_packet);
+
+    storage_.erase(sliced_packet);
+    return ret;
+  }
 
  private:
   /// Underlying storage
