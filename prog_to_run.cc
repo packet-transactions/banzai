bc3f7802b8b6dd5a36f8b937831bc04e200d63f8
Anirudh Sivaraman <sk.anirudh@gmail.com>
2016-03-06 23:00:57 -0500

    Doesn't need to be called test_fields. I don't think this is used anywhere.

--- prog_to_run.cc
+++ prog_to_run.cc
@@ -4,6 +4,6 @@
 extern "C" {
 void update(const Packet & packet __attribute__((unused)), Atom::StateType & state) { state++; }
 int initialize(const Packet & packet __attribute__((unused))) { return 0; }
-PacketFieldSet test_fields({"arrival_time", "dport", "sport"});
-Pipeline test_pipeline{{Atom(test_fields, update, initialize)}};
+PacketFieldSet match_fields({"arrival_time", "dport", "sport"});
+Pipeline test_pipeline{{Atom(match_fields, update, initialize)}};
 }
